cdc53b74184f832f2b361c5a06889e1aee363d44
jaradus <ethanschmertzler@mac.com>
2014-01-15 15:01:47 -0500

    Individual beers now show

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,8 +1,11 @@
 class BeersController < ApplicationController
 
   def index
+    # Requests items from the database
+    @items = Beer.all
+    # Column Names, excluding those in 'exclude'
     exclude = ["id", "created_at", "updated_at", "api_id", "description"]
-    Beer.table(exclude)
+    @items_header = Beer.column_names - exclude
   end
 
   def new
@@ -20,6 +23,10 @@ class BeersController < ApplicationController
   def edit
     @beer = Beer.find(params[:id])
     @regional_styles = RegionalStyle.all
+  end
+
+  def show
+    @item = Beer.find(params[:id])
   end
 
 end
