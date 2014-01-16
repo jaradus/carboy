dfd71840330d4e59a168a993df8f80e5d6c91623
jaradus <ethanschmertzler@mac.com>
2014-01-16 18:15:34 -0500

    Batch recipe complete

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -37,20 +37,51 @@ class BatchesController < ApplicationController
   end
 
   def update
-    params[:batch].each do |hop|
-      if hop[1]["used"] == "1"
-        hop_id =           hop[1]["hop_id"]
-        batch_id =         params[:id]   
-        hop_id =           hop[1][:hop_id]
-        quantity =         hop[1][:quantity]
-        unit_measure =     hop[1][:unit_measure]
-        minutes_in_boil =  hop[1][:minutes_in_boil]
-        alpha_acid =       hop[1][:alpha_acid]
-        beta_acid =        hop[1][:beta_acid]
-        BatchesHop.create(batch_id: batch_id, hop_id: hop_id, hop_id: hop_id, batch_id: batch_id, hop_id: hop_id, quantity: quantity, unit_measure: unit_measure, minutes_in_boil: minutes_in_boil, alpha_acid: alpha_acid, beta_acid: beta_acid)
+    # params["batch"].keys.each do |key|
+    params["batch"].each do |batch|
+      case
+      when batch[0].start_with?("hop")
+          if batch[1]["used"] == "1"
+            hop_id =           batch[1]["hop_id"]
+            batch_id =         params[:id]   
+            hop_id =           batch[1][:hop_id]
+            quantity =         batch[1][:quantity]
+            unit_measure =     batch[1][:unit_measure]
+            minutes_in_boil =  batch[1][:minutes_in_boil]
+            alpha_acid =       batch[1][:alpha_acid]
+            beta_acid =        batch[1][:beta_acid]
+            BatchesHop.create(batch_id: batch_id, hop_id: hop_id, quantity: quantity, unit_measure: unit_measure, minutes_in_boil: minutes_in_boil, alpha_acid: alpha_acid, beta_acid: beta_acid)
+          end
+      when batch[0].start_with?("fermentable")
+          if batch[1]["used"] == "1"
+            fermentable_id  =   batch[1][:fermentable_id]
+            batch_id        =   params[:id]   
+            quantity        =   batch[1][:quantity]
+            unit_measure    =   batch[1][:unit_measure]
+            minutes_in_boil =   batch[1][:minutes_in_boil]
+            BatchesFermentable.create(batch_id: batch_id, fermentable_id: fermentable_id, quantity: quantity, unit_measure: unit_measure, minutes_in_boil: minutes_in_boil)
+          end
+      when batch[0].start_with?("yeast")
+          if batch[1]["used"] == "1"
+            yeast_id        =   batch[1][:yeast_id]
+            batch_id        =   params[:id]   
+            quantity        =   batch[1][:quantity]
+            unit_measure    =   batch[1][:unit_measure]
+            starter =   batch[1][:starter]
+            BatchesYeast.create(batch_id: batch_id, yeast_id: yeast_id, quantity: quantity, unit_measure: unit_measure, starter: starter)
+          end
+      when batch[0].start_with?("specialty")
+          if batch[1]["used"] == "1"
+            specialty_id         =   batch[1][:specialty_id]
+            batch_id             =   params[:id]   
+            quantity             =   batch[1][:quantity]
+            unit_measure         =   batch[1][:unit_measure]
+            minutes_in_kettle    =   batch[1][:minutes_in_kettle]
+            purpose              =   batch[1][:purpose]
+            BatchesSpecialty.create(batch_id: batch_id, specialty_id: specialty_id, quantity: quantity, unit_measure: unit_measure, minutes_in_kettle: minutes_in_kettle, purpose: purpose)
+          end
       end
     end
-
 
     # params = { batch: {
     #     batch_id:         params[:batch_id],
